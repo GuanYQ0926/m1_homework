d8af20bd5cd1e0de29a7ac0e5b633eebad222c6d
GuanYQ0926 <guanyuqing0926@icloud.com>
2017-06-30 00:59:24 +0900

    ...

--- midterm_report/subject_one/src/main.py
+++ midterm_report/subject_one/src/main.py
@@ -3,6 +3,22 @@ import matplotlib.pyplot as plt
 import pywt
 
 
+def diff_points(ys):
+    point_num = 20
+    diff_list = []
+    yss = []
+    xss = []
+    for i in range(len(ys) - 1):
+        diff_list.append(abs(ys[i] - ys[i + 1]))
+    index_list = range(len(ys) - 1)
+    index_list = sorted(index_list, key=lambda i: diff_list[i])
+    for i in range(point_num):
+        temp_index = index_list[-(i + 1)]
+        xss.append(temp_index)
+        yss.append(ys[temp_index])
+    return xss, yss
+
+
 def main1():
     with open('../asset/line-3.csv', 'rb') as f:
         csv_contents = csv.reader(f)
@@ -21,18 +37,7 @@ def main1():
     for i in range(step):
         yss.append(ys[-(step - i)])
     # feature point ysss xsss
-    point_num = 20
-    diff_list = []
-    for i in range(len(ys) - 1):
-        diff_list.append(abs(ys[i] - ys[i + 1]))
-    index_list = range(len(ys) - 1)
-    index_list = sorted(index_list, key=lambda i: diff_list[i])
-    xsss = []
-    ysss = []
-    for i in range(point_num):
-        temp_index = index_list[-(i + 1)]
-        xsss.append(temp_index)
-        ysss.append(ys[temp_index])
+    xsss, ysss = diff_points(ys)
     plt.plot(xs, ys, c='b', lw=0.5)
     plt.plot(xs, yss, c='r', lw=0.5)
     plt.scatter(xsss, ysss, marker='x', c='g')
@@ -47,12 +52,16 @@ def main2():
             contents = row
     xs = range(len(contents))
     ys = [float(i) for i in contents]  # raw data
+    xss, yss = diff_points(ys)
     # yss
     cA, cD = pywt.dwt(ys, 'db2')
+    cx, cy = diff_points(cA)
     plt.figure(1)
     plt.plot(xs, ys, c='b', lw=0.5)
+    plt.scatter(xss, yss, marker='x', c='g')
     plt.figure(2)
     plt.plot(range(len(cA)), cA, c='r', lw=0.5)
+    plt.scatter(cx, cy, marker='x', c='g')
     plt.show()
 
 
